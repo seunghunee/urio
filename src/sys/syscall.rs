174c4f8afa5d8fbbb3536a47c08efbae5cd434c1
Shin Seunghun <seunghunee@gmail.com>
2021-07-18 15:37:01 +0900

    Test io_uring_setup syscall errors

--- src/sys/syscall.rs
+++ src/sys/syscall.rs
@@ -12,3 +12,47 @@ const __NR_io_uring_register: c_long = 427;
 pub unsafe fn io_uring_setup(entries: c_uint, p: *mut io_uring_params) -> c_int {
     syscall(__NR_io_uring_setup, entries, p) as _
 }
+
+#[cfg(test)]
+mod tests {
+    use libc::{EFAULT, EINVAL};
+    use std::{io::Error, ptr};
+
+    use super::*;
+    use crate::sys::IORING_SETUP_SQ_AFF;
+
+    #[test]
+    fn io_uring_setup_no_entries() {
+        let mut p: io_uring_params = Default::default();
+        try_io_uring_setup_err(0, &mut p, EINVAL);
+    }
+    #[test]
+    fn io_uring_setup_null_ptr() {
+        try_io_uring_setup_err(1, ptr::null_mut(), EFAULT);
+    }
+    #[test]
+    fn io_uring_setup_non_zero_resv() {
+        let mut p: io_uring_params = Default::default();
+        p.resv = [1; 3];
+        try_io_uring_setup_err(1, &mut p, EINVAL);
+    }
+    #[test]
+    fn io_uring_setup_invalid_flags() {
+        let mut p: io_uring_params = Default::default();
+        p.flags = u32::MAX;
+        try_io_uring_setup_err(1, &mut p, EINVAL);
+    }
+    #[test]
+    fn io_uring_setup_bind_poll_thread_to_cpu_without_poll_thread() {
+        let mut p: io_uring_params = Default::default();
+        p.flags = IORING_SETUP_SQ_AFF;
+        try_io_uring_setup_err(1, &mut p, EINVAL);
+    }
+
+    fn try_io_uring_setup_err(entries: c_uint, p: *mut io_uring_params, err: c_int) {
+        let ret = unsafe { io_uring_setup(entries, p) };
+        assert_eq!(ret, -1);
+        let raw_os_err = Error::last_os_error().raw_os_error().unwrap();
+        assert_eq!(raw_os_err, err);
+    }
+}
