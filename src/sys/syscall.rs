868a8f57803b79525d9dcdba23394d12a1971318
Shin Seunghun <seunghunee@gmail.com>
2021-07-22 08:22:18 +0900

    Refactor duplicate error assertion code

--- src/sys/syscall.rs
+++ src/sys/syscall.rs
@@ -42,29 +42,29 @@ mod tests {
     #[test]
     fn io_uring_setup_no_entries() {
         let mut p: io_uring_params = Default::default();
-        try_io_uring_setup_err(0, &mut p, EINVAL);
+        assert_err(|| unsafe { io_uring_setup(0, &mut p) }, EINVAL);
     }
     #[test]
     fn io_uring_setup_null_ptr() {
-        try_io_uring_setup_err(1, ptr::null_mut(), EFAULT);
+        assert_err(|| unsafe { io_uring_setup(1, ptr::null_mut()) }, EFAULT);
     }
     #[test]
     fn io_uring_setup_non_zero_resv() {
         let mut p: io_uring_params = Default::default();
         p.resv = [1; 3];
-        try_io_uring_setup_err(1, &mut p, EINVAL);
+        assert_err(|| unsafe { io_uring_setup(1, &mut p) }, EINVAL);
     }
     #[test]
     fn io_uring_setup_invalid_flags() {
         let mut p: io_uring_params = Default::default();
         p.flags = u32::MAX;
-        try_io_uring_setup_err(1, &mut p, EINVAL);
+        assert_err(|| unsafe { io_uring_setup(1, &mut p) }, EINVAL);
     }
     #[test]
     fn io_uring_setup_bind_poll_thread_to_cpu_without_poll_thread() {
         let mut p: io_uring_params = Default::default();
         p.flags = IORING_SETUP_SQ_AFF;
-        try_io_uring_setup_err(1, &mut p, EINVAL);
+        assert_err(|| unsafe { io_uring_setup(1, &mut p) }, EINVAL);
     }
 
     #[test]
@@ -74,16 +74,8 @@ mod tests {
         let mut p: io_uring_params = Default::default();
         p.flags = IORING_SETUP_SQPOLL | IORING_SETUP_SQ_AFF;
         p.sq_thread_cpu = unsafe { sysconf(_SC_NPROCESSORS_CONF) as _ };
-        try_io_uring_setup_err(1, &mut p, EINVAL);
+        assert_err(|| unsafe { io_uring_setup(1, &mut p) }, EINVAL);
     }
-
-    fn try_io_uring_setup_err(entries: c_uint, p: *mut io_uring_params, err: c_int) {
-        let ret = unsafe { io_uring_setup(entries, p) };
-        assert_eq!(ret, -1);
-        let raw_os_err = Error::last_os_error().raw_os_error().unwrap();
-        assert_eq!(raw_os_err, err);
-    }
-
     #[test]
     fn io_uring_setup_read_on_io_uring_fd() {
         let mut p: io_uring_params = Default::default();
@@ -97,22 +89,15 @@ mod tests {
 
     #[test]
     fn io_uring_enter_invalid_fd() {
-        try_io_uring_enter_err(-1, 0, 0, 0, null(), EBADF);
+        assert_err(|| unsafe { io_uring_enter(-1, 0, 0, 0, null()) }, EBADF);
     }
     #[test]
     fn io_uring_enter_valid_non_ring_fd() {
-        try_io_uring_enter_err(0, 0, 0, 0, null(), EOPNOTSUPP);
+        assert_err(|| unsafe { io_uring_enter(0, 0, 0, 0, null()) }, EOPNOTSUPP);
     }
 
-    fn try_io_uring_enter_err(
-        fd: c_int,
-        to_submit: c_uint,
-        min_complete: c_uint,
-        flags: c_uint,
-        sig: *const sigset_t,
-        err: c_int,
-    ) {
-        let ret = unsafe { io_uring_enter(fd, to_submit, min_complete, flags, sig) };
+    fn assert_err(f: impl FnOnce() -> c_int, err: c_int) {
+        let ret = f();
         assert_eq!(ret, -1);
         let raw_os_err = Error::last_os_error().raw_os_error().unwrap();
         assert_eq!(raw_os_err, err);
