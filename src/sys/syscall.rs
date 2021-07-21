5315985347f52afb58a1d96498f566f3488d268c
Shin Seunghun <seunghunee@gmail.com>
2021-07-21 20:57:19 +0900

    Use syscall number of libc crate

--- src/sys/syscall.rs
+++ src/sys/syscall.rs
@@ -1,16 +1,12 @@
 #![allow(non_upper_case_globals)]
 #![allow(clippy::missing_safety_doc)]
 
-use libc::{c_int, c_long, c_uint, syscall};
+use libc::{c_int, c_uint, syscall, SYS_io_uring_setup};
 
 use super::io_uring_params;
 
-const __NR_io_uring_setup: c_long = 425;
-const __NR_io_uring_enter: c_long = 426;
-const __NR_io_uring_register: c_long = 427;
-
 pub unsafe fn io_uring_setup(entries: c_uint, p: *mut io_uring_params) -> c_int {
-    syscall(__NR_io_uring_setup, entries, p) as _
+    syscall(SYS_io_uring_setup, entries, p) as _
 }
 
 #[cfg(test)]
