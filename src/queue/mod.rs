aa0c2128810882d2b36588b5112524ef3a896a8b
Shin Seunghun <seunghunee@gmail.com>
2021-07-30 23:09:09 +0900

    Add queue::mmap for mmap(2)ing io_uring components

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -1,3 +1,34 @@
 pub mod cq;
 pub mod sq;
 mod util;
+
+use std::{io, mem, rc::Rc};
+
+use crate::sys::{
+    io_uring_cqe, io_uring_params, io_uring_sqe, IORING_FEAT_SINGLE_MMAP, IORING_OFF_CQ_RING,
+    IORING_OFF_SQES, IORING_OFF_SQ_RING,
+};
+
+use self::util::Mmap;
+
+pub fn mmap(fd: i32, p: &io_uring_params) -> io::Result<(Rc<Mmap>, Rc<Mmap>, Mmap)> {
+    // mmap rings
+    let sqr_len = p.sq_off.array as usize + p.sq_entries as usize * mem::size_of::<u32>();
+    let cqr_len = p.cq_off.cqes as usize + p.cq_entries as usize * mem::size_of::<io_uring_cqe>();
+
+    let (sqring, cqring) = if p.features & IORING_FEAT_SINGLE_MMAP != 0 {
+        let sqr = Rc::new(Mmap::new(fd, sqr_len.max(cqr_len), IORING_OFF_SQ_RING)?);
+        let cqr = Rc::clone(&sqr);
+        (sqr, cqr)
+    } else {
+        let sqr = Rc::new(Mmap::new(fd, sqr_len, IORING_OFF_SQ_RING)?);
+        let cqr = Rc::new(Mmap::new(fd, cqr_len, IORING_OFF_CQ_RING)?);
+        (sqr, cqr)
+    };
+
+    // mmap sqe array
+    let sqes_len = p.sq_entries as usize * mem::size_of::<io_uring_sqe>();
+    let sqes = Mmap::new(fd, sqes_len, IORING_OFF_SQES)?;
+
+    Ok((sqring, cqring, sqes))
+}
