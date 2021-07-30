7bb8025c72246190b480d6990537c5f4cd2b56ec
Shin Seunghun <seunghunee@gmail.com>
2021-07-30 12:42:11 +0900

    Add top level io_uring interface (Uring)

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,19 @@
 mod queue;
 mod sys;
+
+use queue::{cq::Cq, sq::Sq};
+
+/// io_uring interface.
+pub struct Uring {
+    fd: i32,
+    sq: Sq,
+    cq: Cq,
+    flags: u32,
+    features: u32,
+}
+
+impl Drop for Uring {
+    fn drop(&mut self) {
+        unsafe { libc::close(self.fd) };
+    }
+}
