c571b8ef0c13c3c93f18f73836fe82b9f330c362
Shin Seunghun <seunghunee@gmail.com>
2021-08-04 21:29:45 +0900

    Add Packer::new for init packer

--- src/queue/sqe.rs
+++ src/queue/sqe.rs
@@ -4,7 +4,13 @@ use crate::sys::io_uring_sqe;
 pub struct Packer<'a>(&'a mut io_uring_sqe);
 
 impl<'a> Packer<'a> {
+    /// Create a new [`Packer`] with the given mutable reference to SQE.
     pub fn new(sqe: &'a mut io_uring_sqe) -> Self {
+        sqe.flags = 0;
+        sqe.ioprio = 0;
+        sqe.__bindgen_anon_3.rw_flags = 0;
+        sqe.user_data = 0;
+        sqe.__bindgen_anon_4.__pad2 = [0, 0, 0];
         Self(sqe)
     }
 }
