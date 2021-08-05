d3b100de2b5a3c0d314f7a6991aba3f141c3d1c1
Shin Seunghun <seunghunee@gmail.com>
2021-08-05 20:15:48 +0900

    Add Packer::packup_poll_add for IORING_OP_POLL_ADD

--- src/queue/sqe.rs
+++ src/queue/sqe.rs
@@ -1,4 +1,4 @@
-use crate::sys::io_uring_sqe;
+use crate::sys::{io_uring_sqe, IORING_OP_POLL_ADD};
 
 /// Pack data into a SQE(Submission Queue Entry).
 pub struct Packer<'a>(&'a mut io_uring_sqe);
@@ -26,5 +26,33 @@ impl<'a> Packer<'a> {
     pub fn flags(&mut self, flags: u8) -> &mut Self {
         self.0.flags = flags;
         self
+    }
+
+    /// Pack up data for the operation that poll the specified `fd` for the
+    /// `events`.
+    ///
+    /// Unlike poll or epoll without `EPOLLONESHOT`, this interface always
+    /// works in one shot mode. That is, once the poll operation is
+    /// completed, it will have to be resubmitted. This command works like
+    /// an async `poll(2)` and the completion event result is the returned
+    /// mask of events.
+    #[allow(unused_mut)]
+    #[inline]
+    pub fn packup_poll_add(&mut self, fd: i32, mut events: u32) {
+        self.pack(IORING_OP_POLL_ADD, fd, 0, 0, 0);
+        #[cfg(target_endian = "big")]
+        {
+            events = events << 16 | events >> 16;
+        }
+        self.0.__bindgen_anon_3.poll32_events = events;
+    }
+
+    #[inline]
+    fn pack(&mut self, opcode: u8, fd: i32, addr: u64, len: u32, offset: u64) {
+        self.0.opcode = opcode;
+        self.0.fd = fd;
+        self.0.__bindgen_anon_1.off = offset;
+        self.0.__bindgen_anon_2.addr = addr;
+        self.0.len = len;
     }
 }
