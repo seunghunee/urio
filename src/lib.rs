f2af7c1819ff27b24e45abc0b104948f9ac3070e
Shin Seunghun <seunghunee@gmail.com>
2021-08-11 20:35:29 +0900

    Add Uring::{submit, submit_and_wait} for sqe submission

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@ mod builder;
 mod queue;
 mod sys;
 
-use std::io;
+use std::{io, ptr};
 
 pub use builder::Builder;
 pub use queue::sqe;
 use queue::{cq::Cq, sq::Sq, sqe::Packer};
+use sys::{
+    io_uring_enter, IORING_ENTER_GETEVENTS, IORING_ENTER_SQ_WAKEUP, IORING_SETUP_IOPOLL,
+    IORING_SETUP_SQPOLL,
+};
 
 /// io_uring interface.
 pub struct Uring {
@@ -37,6 +41,52 @@ impl Uring {
     /// If the SQ is full, then an error is returned.
     pub fn alloc_sqe(&mut self) -> Result<Packer, &'static str> {
         self.sq.alloc_sqe()
+    }
+
+    /// Submit pending sqes in the SQ ring to the kernel.
+    /// Returns number of sqes submitted.
+    pub fn submit(&mut self) -> io::Result<usize> {
+        self.submit_and_wait(0)
+    }
+
+    /// Like [`submit`], but allows waiting for events as well.
+    /// Returns number of sqes submitted.
+    ///
+    /// [`submit`]: method@Self::submit
+    pub fn submit_and_wait(&mut self, min_complete: u32) -> io::Result<usize> {
+        let mut flags = 0;
+        let to_submit = self.sq.flush();
+
+        if self.has_sqpoll() {
+            if self.sq.needs_wakeup() {
+                flags |= IORING_ENTER_SQ_WAKEUP;
+            } else if min_complete == 0 {
+                return Ok(to_submit as _);
+            }
+        }
+
+        if min_complete > 0 || self.is_io_polled() {
+            flags |= IORING_ENTER_GETEVENTS;
+        }
+
+        let ret = unsafe { io_uring_enter(self.fd, to_submit, min_complete, flags, ptr::null()) };
+        if ret < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(ret as _)
+    }
+
+    /// Return `true` if IO polling is utilized.
+    #[inline]
+    fn is_io_polled(&self) -> bool {
+        self.flags & IORING_SETUP_IOPOLL != 0
+    }
+
+    /// Return `true` if the kernel side SQ polling thread exist.
+    #[inline]
+    fn has_sqpoll(&self) -> bool {
+        self.flags & IORING_SETUP_SQPOLL != 0
     }
 }
 
