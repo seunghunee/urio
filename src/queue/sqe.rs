9615affc34f09af5c4c9a110a329554e12361cc7
Shin Seunghun <seunghunee@gmail.com>
2021-08-19 22:10:17 +0900

    Make Packer::new pub(crate)

--- src/queue/sqe.rs
+++ src/queue/sqe.rs
@@ -26,7 +26,7 @@ pub struct Packer<'a>(&'a mut io_uring_sqe);
 
 impl<'a> Packer<'a> {
     /// Create a new [`Packer`] with the given mutable reference to SQE.
-    pub fn new(sqe: &'a mut io_uring_sqe) -> Self {
+    pub(crate) fn new(sqe: &'a mut io_uring_sqe) -> Self {
         sqe.flags = 0;
         sqe.ioprio = 0;
         sqe.__bindgen_anon_3.rw_flags = 0;
