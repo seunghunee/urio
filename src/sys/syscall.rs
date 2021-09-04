7542cf437c7ca880fae2ae71e92f1105ccb35e3a
Shin Seunghun <seunghunee@gmail.com>
2021-09-04 15:58:55 +0900

    Test io_uring_enter for waiting sq size events

--- src/sys/syscall.rs
+++ src/sys/syscall.rs
@@ -49,13 +49,15 @@ pub unsafe fn enter(
 #[cfg(test)]
 mod tests {
     use std::{
-        io::Error,
+        error::Error,
+        io::{self, IoSliceMut},
+        os::unix::io::AsRawFd,
         ptr::{null, null_mut},
     };
 
     use super::*;
     use crate::{
-        sys::{IORING_SETUP_SQPOLL, IORING_SETUP_SQ_AFF},
+        sys::{IORING_ENTER_GETEVENTS, IORING_SETUP_SQPOLL, IORING_SETUP_SQ_AFF},
         Uring,
     };
 
@@ -107,7 +109,7 @@ mod tests {
         assert!(ret < 0);
     }
 
-    const IORING_MAX_ENTRIES: u32 = 4096;
+    const RING_SIZE: u32 = 128;
     #[test]
     fn io_uring_enter_invalid_fd() {
         assert_err(|| unsafe { enter(-1, 0, 0, 0, null()) }, EBADF);
@@ -118,7 +120,7 @@ mod tests {
     }
     #[test]
     fn io_uring_enter_invalid_flags() {
-        let ring = Uring::new(IORING_MAX_ENTRIES).expect("Failed to build an Uring");
+        let ring = Uring::new(RING_SIZE).expect("Failed to build an Uring");
         assert_err(
             || unsafe { enter(ring.fd, 1, 0, c_uint::MAX, null()) },
             EINVAL,
@@ -126,15 +128,64 @@ mod tests {
     }
     #[test]
     fn io_uring_enter_no_submit_no_flags() {
-        let ring = Uring::new(IORING_MAX_ENTRIES).expect("Failed to build an Uring");
+        let ring = Uring::new(RING_SIZE).expect("Failed to build an Uring");
         let ret = unsafe { enter(ring.fd, 0, 0, 0, null()) };
         assert_eq!(ret, 0);
+    }
+    const BLOCK_SIZE: usize = 4096;
+    #[test]
+    fn io_uring_enter_wait_sq_size_cqes() -> Result<(), Box<dyn Error>> {
+        let mut ring = Uring::new(RING_SIZE)?;
+
+        // Setup File
+        let sq_capacity = ring.sq_capacity();
+        let file_len = sq_capacity * BLOCK_SIZE;
+        let tmpfile = tempfile::tempfile()?;
+        tmpfile.set_len(file_len as _)?;
+
+        // Allocate sqes as much as the ring could hold.
+        let mut bufs = Vec::with_capacity(sq_capacity);
+        for i in 0..sq_capacity {
+            let mut buf = [0; BLOCK_SIZE];
+            ring.alloc_sqe()?.packup_read_vectored(
+                tmpfile.as_raw_fd(),
+                &mut [IoSliceMut::new(&mut buf)],
+                i as u64 * BLOCK_SIZE as u64,
+            );
+            bufs.push(buf);
+        }
+
+        // Submit the I/Os
+        let submitted = ring.submit()?;
+        assert_eq!(submitted, sq_capacity);
+
+        // Wait for all events
+        let ret = unsafe {
+            enter(
+                ring.as_raw_fd(),
+                0,
+                sq_capacity as _,
+                IORING_ENTER_GETEVENTS,
+                null(),
+            )
+        };
+        assert!(ret == 0);
+        assert_eq!(ring.cq_len(), sq_capacity);
+
+        // Reap cqes
+        let reaper = ring.reap_exact_cqes(sq_capacity)?;
+        for cqe in reaper {
+            let ret = cqe.result()?;
+            assert_eq!(ret, BLOCK_SIZE as _);
+        }
+
+        Ok(())
     }
 
     fn assert_err(f: impl FnOnce() -> c_int, err: c_int) {
         let ret = f();
         assert_eq!(ret, -1);
-        let raw_os_err = Error::last_os_error().raw_os_error().unwrap();
+        let raw_os_err = io::Error::last_os_error().raw_os_error().unwrap();
         assert_eq!(raw_os_err, err);
     }
 }
