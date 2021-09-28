18e9850c71f89c930cfaa78cf123b5cc80ebf482
Shin Seunghun <seunghunee@gmail.com>
2021-09-28 19:30:55 +0900

    Add Registrar::unregister_buffers

--- src/register.rs
+++ src/register.rs
@@ -1,10 +1,11 @@
 use std::{
     io::{self, IoSlice},
+    ptr,
     sync::Arc,
 };
 
 use crate::{
-    sys::{self, IORING_REGISTER_BUFFERS},
+    sys::{self, IORING_REGISTER_BUFFERS, IORING_UNREGISTER_BUFFERS},
     Uring,
 };
 
@@ -26,6 +27,17 @@ impl Registrar {
                 bufs.as_ptr() as _,
                 bufs.len() as _,
             )
+        };
+        if ret < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(())
+    }
+
+    pub fn unregister_buffers(&self) -> io::Result<()> {
+        let ret = unsafe {
+            sys::io_uring_register(self.uring.fd, IORING_UNREGISTER_BUFFERS, ptr::null(), 0)
         };
         if ret < 0 {
             return Err(io::Error::last_os_error());
