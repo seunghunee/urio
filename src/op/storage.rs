70c9d2ce4ce497729408e365167df4b7af7f00f0
Shin Seunghun <seunghunee@gmail.com>
2021-10-15 22:51:52 +0900

    Simplify UnpackerStorage

--- src/op/storage.rs
+++ src/op/storage.rs
@@ -1,75 +1,39 @@
-use parking_lot::Mutex;
-use std::{mem, sync::Arc};
+use std::mem;
 
 use crate::Reaper;
 
-enum Slot {
-    Vacant,
-    Reserved,
-    Occupied(Box<dyn Unpack>),
-}
+pub(super) type Id = u64;
 
-pub(super) type Id = usize;
-
-pub(crate) struct UnpackerStorage {
-    slots: Vec<Slot>,
-    reusable_ids: Vec<Id>,
-}
+pub(crate) struct UnpackerStorage(Vec<Option<Box<dyn Unpack>>>);
 
 impl UnpackerStorage {
     #[inline]
     pub(crate) fn with_capacity(capacity: usize) -> Self {
-        Self {
-            slots: Vec::with_capacity(capacity),
-            reusable_ids: Vec::with_capacity(capacity),
-        }
+        Self(Vec::with_capacity(capacity))
     }
 
     #[inline]
-    fn ticket(&mut self) -> Ticket {
-        let id = self.reusable_ids.pop().unwrap_or_else(|| {
-            self.slots.push(Slot::Vacant);
-            self.slots.len() - 1
-        });
-        self.slots[id] = Slot::Reserved;
-        Ticket { storage: self, id }
+    pub(crate) fn issue_id(&mut self) -> Option<Id> {
+        self.0
+            .iter()
+            .position(|slot| slot.is_none())
+            .map(|id| id as _)
     }
 
     #[inline]
     fn store(&mut self, unpacker: Box<dyn Unpack>) {
         let id = unpacker.id();
-        self.slots[id] = Slot::Occupied(unpacker);
+        self.0[id as usize] = Some(unpacker);
     }
 
     fn release(&mut self, reaper: Reaper) {
         for cqe in reaper {
-            let id = cqe.user_data() as Id;
-            let state = mem::replace(&mut self.slots[id], Slot::Vacant);
-            if let Slot::Occupied(unpacker) = state {
+            let id = cqe.user_data() as usize;
+            let state = mem::replace(&mut self.0[id], None);
+            if let Some(unpacker) = state {
                 unpacker.unpack();
-                self.slots[id] = Slot::Vacant;
             }
         }
-    }
-}
-
-pub(crate) struct Ticket {
-    storage: Arc<Mutex<UnpackerStorage>>,
-    id: Id,
-}
-
-impl Ticket {
-    #[inline]
-    pub(super) fn id(&self) -> Id {
-        self.id
-    }
-}
-
-impl Drop for Ticket {
-    fn drop(&mut self) {
-        let storage = self.storage.lock();
-        storage.slots[self.id] = Slot::Vacant;
-        storage.reusable_ids.push(self.id);
     }
 }
 
